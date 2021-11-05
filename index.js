96b5f137045f8066b1591e7f5135ed36cdbf902f
Julius Garrido <jflgarrido@gmail.com>
2021-11-04 19:59:48 -0600

    commit tree 6965f221460469e9bb7db70e16bec346b9f2dcad

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution in this file!
+/*
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'roksana'
+
+function upperCaseCustomerName() {
+    function upperCaseCustomerName() {
+        function setBestCustomer() {
+            var bestCustomer = 'not bob';
+            function overwriteBestCustomer() {
+                var bestCustomer = 'maybe bob';
+                function changeLeastFavoriteCustomer() {
+                    var leastFavoriteCustomer = 'lex'
+                    return leastFavoriteCustomer;
+                }
+                return bestCustomer;
+            }
+            return bestCustomer;
+        }
+        return customerName.toUpperCase();
+    }
+    return customerName.toUpperCase();
+}
+*/
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'roksana'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'lex'
+    return leastFavoriteCustomer;
+}
